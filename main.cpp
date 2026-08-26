21ffd090a4f7032b0b825b3d8f0fad0ff057e040
Ind0Pacific <vashishtdeepanshu87@gmail.com>
2026-08-26 22:20:22 +0530

    Added SSL support

--- main.cpp
+++ main.cpp
@@ -1,58 +1,66 @@
 #define ASIO_STANDALONE
 #include <asio.hpp>
+#include <asio/ssl.hpp>
 #include <iostream>
 #include <string>
 
-int main()
-{
-    using namespace std;
-    try
-    {
-        asio::io_context io_context;
+int main() {
+  using namespace std;
 
-        std::string input_domain;
-        cout << "Enter the domain name eg www.google.com" << endl;
-        std::getline(std::cin, input_domain);
+  try {
+    asio::io_context io_context;
+    asio::error_code error;
+    asio::streambuf response;
+    std::string input_domain;
 
-        if (input_domain.empty())
-        {
-            cout << "Error: Enter the domain" << endl;
-            return -1;
-        }
 
-        asio::ip::tcp::resolver resolver(io_context);
-        asio::ip::tcp::resolver::results_type endPoints =
-            resolver.resolve(input_domain, "80");
+    cout << "Enter the domain name eg www.google.com" << endl;
+    std::getline(std::cin, input_domain);
 
-        asio::ip::tcp::socket Socket(io_context);
-        asio::connect(Socket, endPoints);
-
-        std::string request = "GET / HTTP/1.1\r\n"
-                              "Host: " +
-                              input_domain +
-                              "\r\n"
-                              "Accept: text/html\r\n"
-                              "Connection: close\r\n\r\n";
-
-        asio::write(Socket, asio::buffer(request));
-
-        asio::streambuf response;
-        asio::error_code error;
-
-        while (asio::read(Socket, response, asio::transfer_at_least(1), error))
-        {
-            cout << &response;
-        }
-
-        if (error != asio::error::eof)
-        {
-            throw asio::system_error(error);
-        }
-    }
-    catch (std::exception &e)
-    {
-        std::cerr << "Network Error: " << e.what() << "\n";
+    if (input_domain.empty()) {
+      cout << "Error: Enter the domain" << endl;
+      return -1;
     }
 
-    return 0;
+    // DNS resolving
+    asio::ip::tcp::resolver resolver(io_context);
+    auto endPoints = resolver.resolve(input_domain, "443" , error);
+
+    if (error) {
+      cout << "DNS Resolution failed: " << error.message() << endl;
+      return -1;
+    }
+
+    //SSL context
+    asio::ssl::context ctx(asio::ssl::context::tls_client);
+    ctx.set_default_verify_paths();
+
+    asio::ssl::stream<asio::ip::tcp::socket> secureSocket(io_context , ctx);
+    SSL_set_tlsext_host_name(secureSocket.native_handle(), input_domain.c_str()); //SNI
+
+    asio::connect(secureSocket.lowest_layer(), endPoints);
+    secureSocket.handshake(asio::ssl::stream_base::client);
+
+    std::string request = "GET / HTTP/1.1\r\n"
+                          "Host: " +
+                          input_domain +
+                          "\r\n"
+                          "Accept: text/html\r\n"
+                          "Connection: close\r\n\r\n";
+
+    asio::write(secureSocket, asio::buffer(request) ); 
+
+    while (asio::read(secureSocket, response, asio::transfer_at_least(1), error)) {
+      cout << &response;
+    }
+
+    if (error != asio::error::eof && error != asio::ssl::error::stream_truncated) {
+      throw asio::system_error(error);
+    }
+
+  } catch (std::exception &e) {
+    std::cerr << "Network Error: " << e.what() << "\n";
+  }
+
+  return 0;
 }
