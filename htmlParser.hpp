a20a07c811b2bcbbf95b2962688c157f1c602955
Ind0Pacific <vashishtdeepanshu87@gmail.com>
2026-09-01 22:39:15 +0530

    Minor improvements.

--- htmlParser.hpp
+++ htmlParser.hpp
@@ -11,12 +11,25 @@ struct DOMNode {
 };
 
 class htmlParser {
+private:
+    static std::string decodeEntities(std::string text) {
+        size_t pos = 0;
+        while ((pos = text.find("&amp;")) != std::string::npos) text.replace(pos, 5, "&");
+        while ((pos = text.find("&copy;")) != std::string::npos) text.replace(pos, 6, "©");
+        while ((pos = text.find("&lt;")) != std::string::npos) text.replace(pos, 4, "<");
+        while ((pos = text.find("&gt;")) != std::string::npos) text.replace(pos, 4, ">");
+        while ((pos = text.find("&quot;")) != std::string::npos) text.replace(pos, 6, "\"");
+        while ((pos = text.find("&nbsp;")) != std::string::npos) text.replace(pos, 6, " ");
+        while ((pos = text.find("&#39;")) != std::string::npos) text.replace(pos, 5, "'");
+        return text;
+    }
+
 public:
   static std::vector<DOMNode> parseDOM(const std::string &html) {
     std::vector<DOMNode> dom_tree;
     size_t cursor = 0;
     bool ignore_text = false;
-    std::string current_tag = "span";
+    std::string current_tag = "span"; 
 
     while (cursor < html.length()) {
       size_t openBracket = html.find('<', cursor);
@@ -26,6 +39,7 @@ public:
         text.erase(0, text.find_first_not_of(" \n\r\t")); 
         
         if (!text.empty() && !ignore_text) {
+          text = decodeEntities(text);
           dom_tree.push_back({current_tag, text});
         }
       }
@@ -41,7 +55,7 @@ public:
         if (tagContent[0] == '/') {
           std::string tagName = tagContent.substr(1);
           if (tagName == "script" || tagName == "style") ignore_text = false;
-          current_tag = "span";
+          current_tag = "span"; 
         } else {
           size_t spacePos = tagContent.find(' ');
           std::string tagName = tagContent.substr(0, spacePos);
