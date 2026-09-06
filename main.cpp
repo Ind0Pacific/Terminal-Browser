2bae93b6226bc3119960ee4ccb7b18643c8ba4e9
Ind0Pacific <vashishtdeepanshu87@gmail.com>
2026-09-06 21:49:21 +0530

    Added fetchBinaryImage function.

--- main.cpp
+++ main.cpp
@@ -38,18 +38,66 @@ std::string decodeChunked(const std::string& raw_body) {
         
         std::string hex_str = raw_body.substr(pos, crlf - pos);
         size_t chunk_size = 0;
-        try {
-            chunk_size = std::stoul(hex_str, nullptr, 16);
-        } catch (...) { break; } 
+        try { chunk_size = std::stoul(hex_str, nullptr, 16); } catch (...) { break; } 
         
         if (chunk_size == 0) break;
-        
         pos = crlf + 2; 
         if (pos + chunk_size > raw_body.size()) break;
         decoded += raw_body.substr(pos, chunk_size);
         pos += chunk_size + 2; 
     }
     return decoded.empty() ? raw_body : decoded;
+}
+
+std::vector<char> fetchBinaryImage(std::string img_url, std::string current_host) {
+    std::string host = current_host, path = img_url;
+    if (img_url.find("http") == 0) {
+         parseUrl(img_url, host, path);
+    } else if (img_url.find("/") != 0) {
+         path = "/" + img_url;
+    }
+
+    std::vector<char> binary_data;
+    try {
+        asio::io_context io_context;
+        asio::ip::tcp::resolver resolver(io_context);
+        auto endPoints = resolver.resolve(host, "443");
+        
+        asio::ssl::context ctx(asio::ssl::context::tls_client);
+        ctx.set_default_verify_paths();
+        asio::ssl::stream<asio::ip::tcp::socket> secureSocket(io_context, ctx);
+        SSL_set_tlsext_host_name(secureSocket.native_handle(), host.c_str());
+        
+        asio::connect(secureSocket.lowest_layer(), endPoints);
+        secureSocket.handshake(asio::ssl::stream_base::client);
+
+        std::string request = "GET " + path + " HTTP/1.1\r\nHost: " + host + "\r\nConnection: close\r\n\r\n";
+        asio::write(secureSocket, asio::buffer(request));
+
+        asio::streambuf response;
+        asio::read_until(secureSocket, response, "\r\n\r\n");
+        
+        std::istream response_stream(&response);
+        std::string header_line;
+        bool is_chunked = false;
+        while (std::getline(response_stream, header_line) && header_line != "\r") {
+            if (header_line.find("chunked") != std::string::npos) is_chunked = true;
+        }
+
+        std::stringstream bodyStream;
+        if (response.size() > 0) bodyStream << &response;
+        asio::error_code error;
+        while (asio::read(secureSocket, response, asio::transfer_at_least(1), error)) {
+            bodyStream << &response;
+        }
+        
+        std::string raw_body = bodyStream.str();
+        if (is_chunked) raw_body = decodeChunked(raw_body);
+        
+        binary_data.assign(raw_body.begin(), raw_body.end());
+    } catch (...) {} 
+    
+    return binary_data;
 }
 
 int main() {
@@ -62,10 +110,7 @@ int main() {
     cout << "Enter the domain name eg www.google.com" << endl;
     std::getline(std::cin, input_domain);
 
-    if (input_domain.empty()) {
-      cout << "Error: Enter the domain" << endl;
-      return -1;
-    }
+    if (input_domain.empty()) return -1;
 
     std::string current_host;
     std::string current_path;
@@ -82,25 +127,17 @@ int main() {
             asio::ip::tcp::resolver resolver(io_context);
             auto endPoints = resolver.resolve(current_host, "443", error);
 
-            if (error) {
-              cout << "DNS Resolution failed: " << error.message() << endl;
-              break; 
-            }
+            if (error) break; 
 
             asio::ssl::context ctx(asio::ssl::context::tls_client);
             ctx.set_default_verify_paths();
-
             asio::ssl::stream<asio::ip::tcp::socket> secureSocket(io_context, ctx);
             SSL_set_tlsext_host_name(secureSocket.native_handle(), current_host.c_str());
 
             asio::connect(secureSocket.lowest_layer(), endPoints);
             secureSocket.handshake(asio::ssl::stream_base::client);
 
-            std::string request = "GET " + current_path + " HTTP/1.1\r\n"
-                                  "Host: " + current_host + "\r\n"
-                                  "Accept: text/html\r\n"
-                                  "Connection: close\r\n\r\n";
-
+            std::string request = "GET " + current_path + " HTTP/1.1\r\nHost: " + current_host + "\r\nConnection: close\r\n\r\n";
             asio::write(secureSocket, asio::buffer(request));
 
             asio::streambuf response;
@@ -122,8 +159,7 @@ int main() {
                     redirect_url = header_line.substr(10);
                     redirect_url.erase(redirect_url.find_last_not_of(" \r\n") + 1);
                 }
-                if (header_line.find("Transfer-Encoding: chunked") != std::string::npos || 
-                    header_line.find("transfer-encoding: chunked") != std::string::npos) {
+                if (header_line.find("chunked") != std::string::npos) {
                     is_chunked = true;
                 }
             }
@@ -144,23 +180,25 @@ int main() {
                 htmlBodyStream << &response;
             }
 
-            if (error != asio::error::eof && error != asio::ssl::error::stream_truncated) {
-                throw asio::system_error(error);
-            }
-
             htmlBody = htmlBodyStream.str();
-            
-            if (is_chunked) {
-                htmlBody = decodeChunked(htmlBody);
-            }
+            if (is_chunked) htmlBody = decodeChunked(htmlBody);
             break; 
         }
 
         if (htmlBody.empty()) break; 
 
         std::vector<DOMNode> dom_tree = htmlParser::parseDOM(htmlBody); 
-        std::string full_url = current_host + current_path;
         
+        cout << "[*] Fetching binary images..." << endl;
+        for (auto& node : dom_tree) {
+            if (node.is_image && !node.image_url.empty()) {
+                if (node.image_url.find("data:image") == std::string::npos) { 
+                    node.image_data = fetchBinaryImage(node.image_url, current_host);
+                }
+            }
+        }
+
+        std::string full_url = current_host + current_path;
         std::string action = WindowManager::open_browser_window(full_url, dom_tree);
 
         if (action.empty()) {
@@ -171,12 +209,10 @@ int main() {
         if (action.find("NEW_DOMAIN:") == 0) {
             std::string typed_url = action.substr(11);
             parseUrl(typed_url, current_host, current_path);
-            cout << "[*] Navigating to user input: " << typed_url << endl;
         } 
         else if (action.find("LINK:") == 0) {
             std::string clicked_url = action.substr(5);
             clicked_url = cleanUrl(clicked_url); 
-            
             if (clicked_url.find("http") == 0) {
                 parseUrl(clicked_url, current_host, current_path);
             } else if (clicked_url.find("/") == 0) {
